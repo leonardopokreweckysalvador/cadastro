428982c9f2eafe6f29adffffc2484f07c49d67e6
Your Name <you@example.com>
2019-08-27 20:59:44 -0300

    ajuste

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { AlunoComponent } from './aluno/aluno.component';
 import { CursoComponent } from './curso/curso.component';
+import { AlunoService } from './aluno/aluno.service';
+import { CursoService } from './curso/curso.service';
+
 
 @NgModule({
   declarations: [
@@ -12,9 +16,11 @@ import { CursoComponent } from './curso/curso.component';
     CursoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
-  providers: [],
+  providers: [AlunoService, CursoService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
